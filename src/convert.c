df89b64a2f15db89e82640f232338618db3d1d2e
Dave Taht <davet@taht.net>
2023-02-07 12:16:38 -0800

    Outline for conversions
    Right now there is a lot of code in whatever_ms or whatever_us
    
    and I would much rather have it be in ns always, and be able
    to specify what the units were via whatever = 3.2ms.
    
    I found the old units command, but really, I just wanted an
    off the shelf lookup table for mbits/kibibits and ms to ns
    and was too lazy to write it yet.

---
+++ src/convert.c
@@ -0,0 +1,9 @@
+// A bare string without a specifier converts from that
+// to kbit
+u64 convert_kbits(char *val, char *assume) {
+}
+
+// A bare string without a specifier converts from that
+u64 convert_ns(char *val, char *assume) {
+
+}
